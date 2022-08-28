fd23d354cbd4024114c67ce530a2034064c98e4c
Flocky05 <md.rakibul.islam.0501@gmail.com>
2022-08-28 01:10:05 +0000

    added train part

--- js/add.js
+++ js/add.js
@@ -8,7 +8,7 @@ var carObject={
 };
 var bikeObject={
     vehecal:'bike',
-    imageUrl:"https://images.unsplash.com/photo-1571188654248-7a89213915f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJpa2V8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    imageUrl:"https://images.unsplash.com/photo-1629294148914-678ba902dd49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vdG9yYmlrZXxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
     farePerKilo:2,
     capacity:2,
     description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
@@ -22,11 +22,35 @@ var busObject={
     description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
 
 };
+var trainObject={
+    vehecal:'train',
+    imageUrl:"https://images.unsplash.com/photo-1515165562839-978bbcf18277?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dHJhaW58ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    farePerKilo:5,
+    capacity:450,
+    description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
+
+};
 var aeroplaneObject={
     vehecal:'aeroplane',
     imageUrl:"https://media.istockphoto.com/photos/3d-rendering-of-the-airport-terminal-picture-id1316256407?b=1&k=20&m=1316256407&s=170667a&w=0&h=Q_GYGCNfVs5ppoUdTGz372s82G-P_pBbKWnxeroq2Ek=",
     farePerKilo:30,
     capacity:300,
+    description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
+
+};
+var boatObject={
+    vehecal:'boat',
+    imageUrl:"https://images.unsplash.com/photo-1569263979104-865ab7cd8d13?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJvYXR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    farePerKilo:4,
+    capacity:35,
+    description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
+
+};
+var smallBoatObject={
+    vehecal:'smallBoat',
+    imageUrl:"https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Ym9hdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+    farePerKilo:4,
+    capacity:5,
     description:"ATPI Halo is a carbon measurement, reduction and offset service designed by travel management experts."
 
 };
@@ -56,5 +80,8 @@ function displayService(service){
 }
 displayService(busObject);
 displayService(carObject);
-displayService(bikeObject);
 displayService(aeroplaneObject);
+displayService(bikeObject);
+displayService(boatObject);
+displayService(smallBoatObject);
+displayService(trainObject);
